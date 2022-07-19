51f473a63842ca8911f7db197b51c7d343b35355
Adimchi <oraegbunamadimchi@gmail.com>
2022-07-19 19:49:49 +0100

    styled headings with tailwind utility classes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Search from './Search';
 
 function App() {
   return (
-    <div className="App container rounded mt-5">
-      <h1  className=''> What word are you looking for? 👀</h1>
+    <div className="App container text-center bg-indigo-400 mt-11">
+      <h1  className=' text-3xl'> What word are you looking for? 👀</h1>
      <Search />
     </div>
   );
