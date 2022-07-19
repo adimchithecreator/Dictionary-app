00f5bdd029c212dc8979a249526bc8187c4ec313
Adimchi <oraegbunamadimchi@gmail.com>
2022-07-19 16:04:17 +0100

    displayed word meanings with API integration

--- src/Search.js
+++ src/Search.js
@@ -1,30 +1,51 @@
 import React, { useState } from "react";
 import './Search.css'
 import axios from "axios";
+import Results from "./Results";
 
-export default function Search() {
+export default function Search(props) {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
-    function wordSearch(event) {
-        event.preventDefault();
-        alert(`Searching for ${keyword} definition`);
-      
 
-        let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
+  function handleDictionResponse(response) {
+    setResults(response.data[0]);
+  }
+    function wordSearch() {
+
+let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
       
-      axios.get(apiUrl).then()
+      axios.get(apiUrl).then(handleDictionResponse)
       }
+      function handleSubmit(event) {
+        event.preventDefault();
+        wordSearch();
+      }
+
       function handleKeywordChange(event) {
         setKeyword(event.target.value);
       }
+      function load() {
+        setLoaded(true);
+        wordSearch();
+      }
+    
 
-
+      if (loaded){
     return(
+
         <div className="Search mt-5">
-        <form onSubmit={wordSearch}>
-        <input type="search" placeholder="Search for a word" onChange={handleKeywordChange}  class="form-control search-input" />
+        <form onSubmit={handleSubmit}>
+        <input type="search" placeholder="Search for a word" defaultValue={props.defaultKeyword} onChange={handleKeywordChange}  class="form-control search-input" />
             
         </form>
+        <Results results={results} />
         </div>
-    )
+    );
+}
+else {
+  load();
+  return "Loading";
+}
 }
