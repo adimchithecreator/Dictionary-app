e72733fb1ce6284d837075616c501302d6749a09
Adimchi <oraegbunamadimchi@gmail.com>
2022-07-28 15:31:15 +0100

    added a photos component
    Integrated the photos component with pexels API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import './App.css';
 import Search from './Search';
 
 function App() {
   return (
-    <div className="App mx-auto container p-7 mt-5 ">
+    <div className="App max-w-[960px] mx-auto container p-7 mt-5 ">
       <h1  className=' text-3xl text-center uppercase font-bold'>English Dictionary</h1>
     <h2 className=' text-center text-xs  text-green-900'>Definitions from <a className=' font-bold hover:underline' href="https://dictionaryapi.dev/"> Free Dictionary API</a> </h2>
      <Search defaultKeyword= "paper" />
--- src/Search.js
+++ src/Search.js
@@ -1,21 +1,35 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Search(props) {
     let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
 
   function handleDictionResponse(response) {
     setResults(response.data[0]);
   }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
+
     function wordSearch() {
 
 let apiUrl= `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
       
       axios.get(apiUrl).then(handleDictionResponse)
+
+      let pexelsApiKey =
+      "563492ad6f917000010000019173e2825ac74c81ba54e75cb11d81e1";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
       }
       function handleSubmit(event) {
         event.preventDefault();
@@ -47,6 +61,7 @@ suggested words: money, travel, trees, wine, music.
         </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
         </div>
     );
 }
