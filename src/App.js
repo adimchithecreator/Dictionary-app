872889f8dc76af6766371c1759b0778227340c59
Adimchi <oraegbunamadimchi@gmail.com>
2022-07-29 21:42:00 +0100

    reduced text size for mobile view

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
  
     <div className="lg:max-w-[960px] font-TDS mx-auto container mt-14">
-   <Search defaultKeyword= "pole" />
+   <Search defaultKeyword= "wax" />
 
      <footer className=' text-center mt-5 font-bold text-sm'> Coded by Adimchi</footer>
     </div>
